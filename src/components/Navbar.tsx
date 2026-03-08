8c79c5f0252c27703ba3f6b43ffd6fc242d39407
ADaneshS1 <daneshsudiono@gmail.com>
2026-03-08 15:12:45 +0700

    feat: add shadcn components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,41 @@
+import { ShoppingCart } from "lucide-react";
+import { Button } from "./ui/button";
+
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between px-10 py-6 bg-[#121420] text-white">
+    <nav className="flex items-center justify-between px-10 py-6 bg-[#121420] border-b border-slate-800 text-white">
       <div className="flex items-center gap-12">
-        <h1 className="text-2xl font-bold text-blue-500 leading-none">
-          Lego <br /> <span className="text-blue-600">Store</span>
-        </h1>
-        <div className="flex gap-8 text-sm font-medium">
-          <a href="#" className="hover:text-blue-400">
+        <div className="flex flex-col cursor-pointer">
+          <span className="text-xl font-black text-blue-500 uppercase leading-none">
+            Lego
+          </span>
+          <span className="text-xl font-black text-blue-700 uppercase leading-none ml-2">
+            Store
+          </span>
+        </div>
+        <div className="hidden md:flex gap-8 text-sm font-medium text-slate-400">
+          <a href="#" className="hover:text-white transition-colors">
             Games
           </a>
-          <a href="#" className="hover:text-blue-400">
+          <a href="#" className="hover:text-white transition-colors">
             DLC
           </a>
         </div>
       </div>
-      <div className="flex items-center gap-6">
-        <button className="font-semibold hover:text-blue-400">Log In</button>
+      <div className="flex items-center gap-4">
+        <Button
+          variant="ghost"
+          size="icon"
+          className="text-white hover:bg-slate-800"
+        >
+          <ShoppingCart className="w-5 h-5" />
+        </Button>
+        <Button
+          variant="secondary"
+          className="font-bold bg-blue-600 hover:bg-blue-700 text-white border-none"
+        >
+          Log In
+        </Button>
       </div>
     </nav>
   );
